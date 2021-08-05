29a24b20fada6647751ec3304c674acc4ddd9f71
vinicius <vinicius_jose16@hotmail.com>
2021-08-05 11:32:20 -0300

    Rodapé finalizado

--- imobiliaria/src/components/Footer.tsx
+++ imobiliaria/src/components/Footer.tsx
@@ -1,3 +1,6 @@
+import whatsImg from "../assets/icons/whatsapp.png"
+import faceImg from "../assets/icons/facebook.png"
+import instaImg from "../assets/icons/instagram.png"
 import './styles/rodape.scss'
 
 export function Footer() {
@@ -5,7 +8,20 @@ export function Footer() {
     <footer>
       <div className="nav-footer">
         <div className="itens">
-
+          <div className="imagem">LOGO</div>
+          <div className="content-footer">
+            <h1>Redes Sociais</h1>
+            <ul>
+              <li><a href="#"><img src={whatsImg} alt="" /></a></li>
+              <li><a href="#"><img src={faceImg} alt="" /></a></li>
+              <li><a href="#"><img src={instaImg} alt="" /></a></li>
+            </ul>
+          </div>
+        </div>
+        <div className="separator-3"></div>
+        <div className="copyright">
+          <h1>Brasil</h1>
+          <h1>© 2021 LOREN. Todos os direitos reservados.</h1>
         </div>
       </div>
     </footer>
